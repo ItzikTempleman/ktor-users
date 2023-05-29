65174815fb0c9d28594aa2e897b6b1336efc238e
liaddd <liaddd@gmail.com>
2023-05-30 02:12:17 +0300

    fix: ?

--- src/main/kotlin/com/itzik/Application.kt
+++ src/main/kotlin/com/itzik/Application.kt
@@ -2,9 +2,14 @@ package com.itzik
 
 import io.ktor.server.application.*
 import com.itzik.plugins.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
+        EngineMain.main(args)
+    }.start(wait = true)
+}
 
 fun Application.module() {
     configureSerialization()
